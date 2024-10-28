fd5d297236b94bfa4cb27af2fed6b30d9445d772
liaojack8 <liaojack8.tw@gmail.com>
2024-10-28 14:45:18 +0800

    use MouseEvent to click element

--- chiikawa-market-better.user.js
+++ chiikawa-market-better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chiikawa Market Better
 // @namespace    https://github.com/liaojack8
-// @version      v20241027.2
+// @version      v20241028.1
 // @author       liaojack8
 // @description  Check storage of products in Chiikawa market.
 // @homepage     https://github.com/liaojack8/chiikawa-market-better
@@ -19,32 +19,44 @@
 
 (function () {
     "use strict";
-    function fuckWorldshopping() {
-        // remove worldshopping
-        document.querySelector('#zigzag-worldshopping-checkout').remove();
-        // auto agree product_attetion
-        document.querySelector("div.agree_box>input").click();
-    }
-    if (document.location.pathname === "/cart") {
-        // Cart.
-        for (const item of document.getElementsByClassName("cart--item")) {
-            const quantity = item.getAttribute("data-inventory-quantity");
-            const label =
-                item.getElementsByClassName("cart--item--title")?.[0]?.children?.[0]
-                    ?.children?.[0];
+    function execute() {
+        if (document.location.pathname === "/cart") {
+            // Cart.
+            for (const item of document.getElementsByClassName("cart--item")) {
+                const quantity = item.getAttribute("data-inventory-quantity");
+                const label =
+                    item.getElementsByClassName("cart--item--title")?.[0]?.children?.[0]
+                        ?.children?.[0];
+                if (quantity !== undefined && label) {
+                    label.innerHTML += `<span style="color: red; font-weight: bold;"> [庫存: ${quantity}]</span>`;
+                }
+                // auto agree terms
+                document.querySelector("div.checkArea>label").dispatchEvent(clickEvent);
+                document.querySelector("div.checkArea>input").checked = true;
+            }
+        } else if (document.location.pathname.includes('products/')) {
+            // Product.
+            const quantity = document
+                .getElementsByClassName("product-form--variant-select")?.[0]
+                ?.children?.[0]?.getAttribute("data-inventory-quantity");
+            const label = document.getElementsByClassName("product-page--title")?.[0];
             if (quantity !== undefined && label) {
                 label.innerHTML += `<span style="color: red; font-weight: bold;"> [庫存: ${quantity}]</span>`;
             }
+            // auto agree product_attetion
+            document.querySelector("div.agree_box>label").dispatchEvent(clickEvent);
+            document.querySelector("div.agree_box>input").checked = true;
         }
-    } else {
-        // Product.
-        const quantity = document
-            .getElementsByClassName("product-form--variant-select")?.[0]
-            ?.children?.[0]?.getAttribute("data-inventory-quantity");
-        const label = document.getElementsByClassName("product-page--title")?.[0];
-        if (quantity !== undefined && label) {
-            label.innerHTML += `<span style="color: red; font-weight: bold;"> [庫存: ${quantity}]</span>`;
-        }
+        setTimeout(fuckWorldshopping, 1000)
     }
-    window.addEventListener('load', setTimeout(fuckWorldshopping, 2000));
+    var clickEvent = new MouseEvent('click', {
+        'view': window,
+        'bubbles': true,
+        'cancelable': true
+    });
+    function fuckWorldshopping() {
+        // remove worldshopping
+        document.querySelector('#zigzag-worldshopping-checkout').remove();
+    }
+    window.addEventListener('load', setTimeout(execute, 1000));
 })();
